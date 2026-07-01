ad582b14e29f32c9468cd1c7102121f7712e9202
CrisbyRF <157751250+CrisbyRF@users.noreply.github.com>
2026-07-01 09:37:47 -0400

    Se eliminaron importaciones que no se utilizaban

--- src/models/pedido.py
+++ src/models/pedido.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from datetime import datetime, timezone
-from typing import List, Optional
+from typing import List
 
 from models.base_model import RepositorioBase
 
